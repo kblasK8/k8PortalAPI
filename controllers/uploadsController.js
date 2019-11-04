178d7dba7dc66b3e6b367ec4a316980c2340cbfa
Jayson Pamittan <jayson.pamittan@gmail.com>
2019-11-05 03:19:37 +0800

    Remove console log.

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,6 +2,5 @@ const config = require('../config/config');
 
 exports.get_file = function(req, res) {
   const file = config.uploadPath + req.params.downloadFile;
-  console.log(file);
   res.download(file);
 };
